614d020df1bb6f8731635c790dc64a23806a38c2
huafeng <1433405285@qq.com>
2017-03-09 23:27:33 +0800

    修复导航栏bug

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,7 +2,8 @@
 namespace app\index\controller;
 use think\View;
 use think\Controller;
-
+$request = \think\Request::instance();
+define('ACTION_NAME', $request->action());
 class Index
 {
     //打印首页
@@ -15,7 +16,7 @@ class Index
         return $view->fetch('index');
     }
 
-    //打印首页
+    //打印详情页
     public function detail()
     {
         $apps=db('apps')->where('AppStatus',1)->select();
@@ -23,6 +24,16 @@ class Index
         $view = new View();
         $view->assign('apps',$apps);
         return $view->fetch('Detail/detail');
+    }
+
+    //打印分类页
+    public function category()
+    {
+        $apps=db('apps')->where('AppStatus',1)->select();
+        //var_dump($apps);
+        $view = new View();
+        $view->assign('apps',$apps);
+        return $view->fetch('Category/category');
     }
 
 }
