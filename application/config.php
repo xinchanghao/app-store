4d8434438a4df3637a085d4b9d60c9671838f25e
huafeng <1433405285@qq.com>
2017-04-11 19:04:54 +0800

    public_html更改

--- application/config.php
+++ application/config.php
@@ -136,8 +136,8 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'  =>  [
-        '__PUBLIC__'=>'/public/',
-        '__ROOT__' => '/',
+        '__STATIC__'=>'/public_html/static',
+       // '__ROOT__' => '/',
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
